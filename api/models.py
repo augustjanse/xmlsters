9d79c794082d927237ad292272d4c3e90a53aff8
August Janse <augustj@kth.se>
2018-01-16 17:27:48 +0100

    Fix broken code in models and tests

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Chart(models.Model):
-    placement = models.IntegerField
+    placement = models.IntegerField(default=-1)
     mbid = models.CharField(max_length=36)
 
 
--- api/tests.py
+++ api/tests.py
@@ -1,12 +1,22 @@
 from django.test import TestCase
 
+from api.models import Chart
+from api.models import User
+
 
 class ChartTestCase(TestCase):
     def setUp(self):
         Chart.objects.create(placement=1, mbid="19a8b6e5-f753-36c6-a3c6-189390d09935")
         Chart.objects.create(placement=2, mbid="86949026-de16-34f1-8d8a-1d662ed8c0bb")
 
+    def test_nothing(self):
+        pass
+
 
 class UserTestCase(TestCase):
     def setUp(self):
-        User.objects.create(chart_id="abc123")
+        Chart.objects.create(placement=1, mbid="19a8b6e5-f753-36c6-a3c6-189390d09935")
+        User.objects.create(chart_id=Chart.objects.get(placement=1))
+
+    def test_nothing(self):
+        print(User.objects.all())
