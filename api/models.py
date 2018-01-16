2e1d4c6f40a89e6370c132a3877eb69991f9a23c
August Janse <augustj@kth.se>
2018-01-16 16:40:51 +0100

    Fill models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Chart(models.Model):
+    placement = models.IntegerField
+    mbid = models.CharField(max_length=36)
+
+
+class User(models.Model):
+    chart_id = models.ForeignKey(Chart, on_delete=models.CASCADE)
