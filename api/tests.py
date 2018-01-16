da01a0af94bbd303399e9e31a7dc207f6f28ddb8
August Janse <augustj@kth.se>
2018-01-16 18:16:18 +0100

    Replace tests with slightly more useful test

--- api/tests.py
+++ api/tests.py
@@ -4,19 +4,12 @@ from api.models import Chart
 from api.models import User
 
 
-class ChartTestCase(TestCase):
+class ModelTestCase(TestCase):
     def setUp(self):
         Chart.objects.create(placement=1, mbid="19a8b6e5-f753-36c6-a3c6-189390d09935")
         Chart.objects.create(placement=2, mbid="86949026-de16-34f1-8d8a-1d662ed8c0bb")
-
-    def test_nothing(self):
-        pass
-
-
-class UserTestCase(TestCase):
-    def setUp(self):
-        Chart.objects.create(placement=1, mbid="19a8b6e5-f753-36c6-a3c6-189390d09935")
         User.objects.create(chart_id=Chart.objects.get(placement=1))
 
-    def test_nothing(self):
-        print(User.objects.all())
+    def test_number_of_objects(self):
+        self.assertEqual(Chart.objects.all().count(), 2)
+        self.assertEqual(User.objects.all().count(), 1)
