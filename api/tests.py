6999b5687ecccaf205d8eaa07100020929fdaf88
August Janse <augustj@kth.se>
2018-01-17 09:22:28 +0100

    Add more DRF tests

--- api/tests.py
+++ api/tests.py
@@ -31,3 +31,29 @@ class ViewTestCase(TestCase):
 
     def test_can_create_chart(self):
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+
+    def test_can_get_chart(self):
+        chart = Chart.objects.get()
+        response = self.client.get(
+            reverse('details', kwargs={'placement': chart.placement}), format="json"
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, chart)
+
+    def test_can_update_chart(self):
+        chart = {'placement': 12}
+        response = self.client.put(
+            reverse('details', kwargs={'placement': Chart.objects.get().placement}),
+            chart, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_can_delete_chart(self):
+        chart = Chart.objects.get()
+        response = self.client.put(
+            reverse('details', kwargs={'placement': chart.placement}),
+            chart, format='json', follow=True
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
