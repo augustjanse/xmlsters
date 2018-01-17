73bd3368c4d71878cae6b0bd190da9e4993dba95
August Janse <augustj@kth.se>
2018-01-17 11:45:37 +0100

    Remove tests

--- api/tests.py
+++ api/tests.py
@@ -36,19 +36,12 @@ class ViewTestCase(TestCase):
             reverse('details', kwargs={'pk': chart.pk})
         )
 
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertContains(response, chart)
-
-    def test_can_update_chart(self):
-        chart = {'placement': 12}
-        response = self.client.put(
-            reverse('details', kwargs={'pk': Chart.objects.get().pk}), chart
-        )
+        # Should test contents of response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_can_delete_chart(self):
         chart = Chart.objects.get()
-        response = self.client.put(
+        response = self.client.delete(
             reverse('details', kwargs={'pk': chart.pk}), follow=True
         )
 
