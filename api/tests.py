6ef05c9b886266f7cc816a8340aa4d50b975d9c9
August Janse <augustj@kth.se>
2018-01-17 09:58:57 +0100

    Change tests a bit

--- api/tests.py
+++ api/tests.py
@@ -11,12 +11,11 @@ class ModelTestCase(TestCase):
     def setUp(self):
         Chart.objects.create(placement=1, mbid="19a8b6e5-f753-36c6-a3c6-189390d09935")
         Chart.objects.create(placement=2, mbid="86949026-de16-34f1-8d8a-1d662ed8c0bb")
-        User.objects.create(chart_id=Chart.objects.get(placement=1))
+        User.objects.create(chart_id=Chart.objects.get(pk=1))
 
     def test_number_of_objects(self):
         self.assertEqual(Chart.objects.all().count(), 2)
         self.assertEqual(User.objects.all().count(), 1)
-
 
 class ViewTestCase(TestCase):
     def setUp(self):
@@ -35,7 +34,7 @@ class ViewTestCase(TestCase):
     def test_can_get_chart(self):
         chart = Chart.objects.get()
         response = self.client.get(
-            reverse('details', kwargs={'placement': chart.placement}), format="json"
+            reverse('details', kwargs={'pk': chart.pk}), format="json"
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -44,7 +43,7 @@ class ViewTestCase(TestCase):
     def test_can_update_chart(self):
         chart = {'placement': 12}
         response = self.client.put(
-            reverse('details', kwargs={'placement': Chart.objects.get().placement}),
+            reverse('details', kwargs={'pk': Chart.objects.get().pk}),
             chart, format='json'
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -52,8 +51,8 @@ class ViewTestCase(TestCase):
     def test_can_delete_chart(self):
         chart = Chart.objects.get()
         response = self.client.put(
-            reverse('details', kwargs={'placement': chart.placement}),
-            chart, format='json', follow=True
+            reverse('details', kwargs={'pk': chart.pk}),
+            format='json', follow=True
         )
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
