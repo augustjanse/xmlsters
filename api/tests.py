2b6cb893de311544e852f83cd0e13a06e1a26901
August Janse <augustj@kth.se>
2018-01-18 15:13:22 +0100

    Remove XML from everywhere

--- api/tests.py
+++ api/tests.py
@@ -1,6 +1,3 @@
-import xml.etree.ElementTree as ET
-
-from defusedxml.ElementTree import parse
 from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
@@ -20,16 +17,35 @@ class ModelTestCase(TestCase):
         self.assertEqual(Chart.objects.all().count(), 2)
         self.assertEqual(User.objects.all().count(), 1)
 
+
 class ViewTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
+
     def test_can_create_chart(self):
-        chart = parse("api/tests/testchart.xml")
+        chart = {
+            "chart": {"head": {
+                "chartid": " 1 ",
+                "userid": " 1 "
+            }, "body": {
+                "release": [
+                    {
+                        "-placement": "1",
+                        "#text": "19a8b6e5-f753-36c6-a3c6-189390d09935"
+                    },
+                    {
+                        "-placement": "2",
+                        "#text": "86949026-de16-34f1-8d8a-1d662ed8c0bb"
+                    }
+                ]
+            }
+            }
+        }
 
         self.response = self.client.post(
             reverse('create'),
-            ET.tostring(chart.getroot()),
-            format='xml'
+            chart,
+            format='json'
         )
 
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 from django.utils.six import BytesIO
 from rest_framework import status
+from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework_xml.parsers import XMLParser
 
 from api.serializers import ChartSerializer
 
@@ -11,7 +11,7 @@ class CreateChart(APIView):
     def post(self, request):
         # http://www.django-rest-framework.org/api-guide/serializers/#deserializing-objects
         stream = BytesIO(request.body)
-        data = XMLParser().parse(stream)
+        data = JSONParser().parse(stream)
         serializer = ChartSerializer(data=data)
         serializer.is_valid()
 
