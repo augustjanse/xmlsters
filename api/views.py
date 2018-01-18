7af32b84d3fc9d1abb519a302e048f7c90a7e9c2
August Janse <augustj@kth.se>
2018-01-18 11:18:44 +0100

    Properly parse XML

--- api/views.py
+++ api/views.py
@@ -1,8 +1,18 @@
+from django.utils.six import BytesIO
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_xml.parsers import XMLParser
+
+from api.serializers import ChartSerializer
 
 
 class CreateChart(APIView):
     def post(self, request):
+        # http://www.django-rest-framework.org/api-guide/serializers/#deserializing-objects
+        stream = BytesIO(request.body)
+        data = XMLParser().parse(stream)
+        serializer = ChartSerializer(data=data)
+        serializer.is_valid()
+
         return Response(status.HTTP_201_CREATED)
