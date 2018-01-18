e56a2830284e45b1cb0e6aab758fc3dec9090464
August Janse <augustj@kth.se>
2018-01-18 17:38:45 +0100

    Write possibly functioning to_representation

--- api/serializers.py
+++ api/serializers.py
@@ -2,6 +2,8 @@ from django.utils.six import BytesIO
 from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 
+from api.models import Chart, User
+
 
 class ChartSerializer(serializers.BaseSerializer):
     def to_internal_value(self, data):
@@ -20,4 +22,24 @@ class ChartSerializer(serializers.BaseSerializer):
 
     def to_representation(self, instance):
         """Serializes internal data into something for the response body."""
-        print("test333")
+        # Assume instance has a chartid somehow, maybe?
+
+        chart_id = instance.id
+
+        charts = Chart.objects.get(pk=chart_id)
+        user = User.objects.get(chart_id=instance)
+
+        user_id = user.pk
+        release = {"release": []}
+        for chart in charts:
+            release["release"].append({"-placement": chart.placement, "#text": chart.mbid})
+
+        return {
+            "chart": {"head": {
+                "chartid": chart_id,
+                "userid": user_id
+            }, "body": release
+            }
+        }
+
+    # http://www.django-rest-framework.org/api-guide/serializers/#writing-create-methods-for-nested-representations
