a83dbedc919dc60f019e8b9b972d17b2ce51ea16
August Janse <augustj@kth.se>
2018-01-24 16:26:11 +0100

    Add form with JS

--- client/js/index.js
+++ client/js/index.js
@@ -1,5 +1,19 @@
 // Simple Javascript file. Due to the specification of the assignment,
 // no frontend frameworks will be used.
 $(function () {
-    console.log(1)
+    // https://stackoverflow.com/a/14472606/1729441
+    $("form").on('submit', function (e) {
+        // Reuses request from old userscript
+        var input = $("#mbid_box").val();
+        var mbid = input.match("[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+-[a-z0-9]+")[0];
+        entity = null;
+        setEntity(input, mbid);
+
+        url = "http://coverartarchive.org/" + entity + "/front";
+        jQuery.get(url, function (data) {
+            alert(data)
+        });
+
+        e.preventDefault()
+    })
 });
